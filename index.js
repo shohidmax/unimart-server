27fa89b9364c2edac40f1f6604ca032217a19123
shohdmax <shohdmax@gmail.com>
2022-07-09 21:51:55 +0600

    now time

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lg5wc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
    
-
-
-
-
+ 
 //fdfadfasdfsdf
 
 async function run() {
